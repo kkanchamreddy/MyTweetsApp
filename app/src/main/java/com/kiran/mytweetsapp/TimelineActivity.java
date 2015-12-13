77f3d9182057a1edba5a25426165915b183aaef3
kkanchamreddy <kiran.kanchamreddy@gmail.com>
2015-12-12 22:46:10 -0800

    Implement pull to refresh

--- app/src/main/java/com/kiran/mytweetsapp/TimelineActivity.java
+++ app/src/main/java/com/kiran/mytweetsapp/TimelineActivity.java
@@ -1,7 +1,10 @@
 package com.kiran.mytweetsapp;
 
 import android.os.Bundle;
+import android.support.v4.widget.SwipeRefreshLayout;
+import android.support.v4.widget.SwipeRefreshLayout.OnRefreshListener;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.widget.ListView;
 
 import com.kiran.mytweetsapp.models.Tweet;
@@ -21,10 +24,32 @@ public class TimelineActivity extends AppCompatActivity {
     private ArrayList<Tweet> tweets;
     private int lastMaxId = 0;
 
+    private SwipeRefreshLayout swipeContainer;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_timeline);
+        swipeContainer = (SwipeRefreshLayout) findViewById(R.id.swipeContainer);
+
+        // Setup refresh listener which triggers new data loading
+        swipeContainer.setOnRefreshListener(new OnRefreshListener() {
+            @Override
+            public void onRefresh() {
+                // Your code to refresh the list here.
+                // Make sure you call swipeContainer.setRefreshing(false)
+                // once the network request has completed successfully.
+                fetchTimelineAsync(0);
+            }
+        });
+        // Configure the refreshing colors
+        swipeContainer.setColorSchemeResources(android.R.color.holo_blue_bright,
+                android.R.color.holo_green_light,
+                android.R.color.holo_orange_light,
+                android.R.color.holo_red_light);
+
+
         client = TwitterApplication.getRestClient();
 
         lvTweets = (ListView)findViewById(R.id.lvTweets);
@@ -38,10 +63,7 @@ public class TimelineActivity extends AppCompatActivity {
         lvTweets.setOnScrollListener(new EndlessScrollListener() {
             @Override
             public boolean onLoadMore(int page, int totalItemsCount) {
-                // Triggered only when new data needs to be appended to the list
-                // Add whatever code is needed to append new items to your AdapterView
-                customLoadMoreDataFromApi();
-                // or customLoadMoreDataFromApi(totalItemsCount);
+                populateTimeline(Tweet.getLastTweetId());
                 return true; // ONLY if more data is actually being loaded; false otherwise.
             }
         });
@@ -49,10 +71,7 @@ public class TimelineActivity extends AppCompatActivity {
         populateTimeline(lastMaxId);
     }
 
-    // Append more data into the adapter
-    public void customLoadMoreDataFromApi() {
-        populateTimeline(Tweet.getLastTweetId());
-    }
+
 
     private void populateTimeline(long maxId) {
         client.getHomeTimeline(maxId, new JsonHttpResponseHandler() {
@@ -68,4 +87,22 @@ public class TimelineActivity extends AppCompatActivity {
 
         });
     }
+
+    public void fetchTimelineAsync(int page) {
+        client.getHomeTimeline(page, new JsonHttpResponseHandler() {
+            public void onSuccess(int statusCode, Header[] headers, JSONArray response) {
+                // Remember to CLEAR OUT old items before appending in the new ones
+                tweetsAdapter.clear();
+                // ...the data has come back, add new items to your adapter...
+                tweetsAdapter.addAll(Tweet.fromJSONArray(response));
+                // Now we call setRefreshing(false) to signal refresh has finished
+                swipeContainer.setRefreshing(false);
+            }
+
+            public void onFailure(Throwable e) {
+                Log.d("DEBUG", "Fetch timeline error: " + e.toString());
+            }
+        });
+    }
+
 }
