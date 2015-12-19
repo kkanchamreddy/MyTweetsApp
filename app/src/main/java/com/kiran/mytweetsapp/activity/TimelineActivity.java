355d5656581c71731fd963c0127550dbbe4b4fe6
kkanchamreddy <kiran.kanchamreddy@gmail.com>
2015-12-19 08:51:03 -0800

    Add ViewPager and PagerSlider to slide between Home & Mention timeline fragments

--- app/src/main/java/com/kiran/mytweetsapp/activity/TimelineActivity.java
+++ app/src/main/java/com/kiran/mytweetsapp/activity/TimelineActivity.java
@@ -1,15 +1,23 @@
 package com.kiran.mytweetsapp.activity;
 
+
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.Toast;
 
+import com.astuetz.PagerSlidingTabStrip;
 import com.kiran.mytweetsapp.R;
 import com.kiran.mytweetsapp.TwitterClient;
+import com.kiran.mytweetsapp.fragments.HomeTimelineFragment;
+import com.kiran.mytweetsapp.fragments.MentionsTimelineFragment;
 
 public class TimelineActivity extends AppCompatActivity {
 
@@ -36,8 +44,19 @@ public class TimelineActivity extends AppCompatActivity {
 
 
         getSupportActionBar().setLogo(R.drawable.ic_twitter_logo);
-        getSupportActionBar().setTitle(R.string.home);
+        getSupportActionBar().setDisplayShowTitleEnabled(false);
 
+        //Get the view pager
+        ViewPager vpPager = (ViewPager)findViewById(R.id.viewpager);
+
+        //Set the view pager adapter for the pager
+        vpPager.setAdapter(new TweetsPagerAdapter(getSupportFragmentManager()));
+
+        //Find the Page sliding tabstrip
+        PagerSlidingTabStrip psTabStrip = (PagerSlidingTabStrip) findViewById(R.id.tabs);
+
+        //Attach the pager tabs to the view pager
+       psTabStrip.setViewPager(vpPager);
 
     }
 
@@ -74,6 +93,42 @@ public class TimelineActivity extends AppCompatActivity {
             //fetchTimelineAsync(0);
             // Toast the name to display temporarily on screen
             Toast.makeText(this, "Tweet success!!", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+
+
+    //Return the order of fragments in the view page
+    public class TweetsPagerAdapter extends FragmentPagerAdapter {
+        private String tabTitles[] = {"Home", "Mentions"};
+
+
+        //Adapter gets the manager to insert or remove fragments from the activity
+        public TweetsPagerAdapter(FragmentManager fm) {
+            super(fm);
+        }
+
+        //The order and creation of fragments with in the pager
+        @Override
+        public Fragment getItem(int position) {
+            if(position == 0) {
+                return new HomeTimelineFragment();
+            } else if(position == 1) {
+                return new MentionsTimelineFragment();
+            }
+            return null;
+        }
+
+        //Returns the tab title
+        @Override
+        public CharSequence getPageTitle(int position) {
+            return tabTitles[position];
+        }
+
+        //How many fragments are there to swipe between
+        @Override
+        public int getCount() {
+            return tabTitles.length;
         }
     }
 
